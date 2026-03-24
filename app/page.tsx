e09f24b9b138d411b13074808acd0bbfd7212e4d
jeremt <taboada.jeremie@gmail.com>
2026-03-24 12:29:11 +0100

    feat: implement user session handling and display user info on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import {auth} from "@/lib/auth";
 import {db} from "@/lib/db/db";
 import {posts} from "@/lib/db/schema";
+import {headers} from "next/headers";
 
 export default async function Home() {
+    const session = await auth.api.getSession({headers: await headers()});
     const postsResult = await db
         .select({
             id: posts.id,
@@ -9,9 +12,15 @@ export default async function Home() {
             description: posts.description,
         })
         .from(posts);
+    if (!session?.user) {
+        return <div>Not connected</div>;
+    }
     return (
         <div className="flex flex-col gap-2 w-2xl max-w-full">
             <h1>Posts</h1>
+            {session.user.id}
+            {session.user.name}
+            {session.user.email}
             {postsResult.map((post) => (
                 <div className="p-4 border w-full" key={post.id}>
                     <h2 className="text-xl">{post.name}</h2>
